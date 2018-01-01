42f7d6dbe9d3d394d8b6590c62035dfb8b3d34cf
lazha <lazha@microsoft.com>
2017-12-31 17:13:43 -0800

    fix callback err

--- node/main.js
+++ node/main.js
@@ -1,16 +1,24 @@
 const Gdax = require('gdax');
 const publicClient = new Gdax.PublicClient();
 
-// Get Product
+// // Get Product
 publicClient
-.getProducts()
+.getProductOrderBook("BTC-USD")
 .then(data => {
-    data
-    && data.length > 0
-    && data.map((v, i) => {
-        console.log("Product " + (i + 1) + ": " + v.display_name)
-    });
+    console.log(data.asks.length);
 })
 .catch(error => {
     console.log("Get gdax product error: " + error);
 });
+
+// // Websocket Client
+// const websocket = new Gdax.WebsocketClient(['BTC-USD', 'ETH-USD', 'LTC-USD']);
+
+// websocket.on('message', data => { console.log("Websocket data: " + `${data.side} ${data.size} ${data.product_id} @ ${data.price} @ ${data.time}`) });
+// websocket.on('error', err => { console.log("Websocket error: " + err) });
+
+// Orderbook sync
+//const orderbook = new Gdax.Orderbook();
+//console.log(Object.keys(orderbook.state()));
+//console.log('LTC-USD' + orderbookSync.books['LTC-USD'].state());
+
