05cb8e2b9f4ffe669568f0c3a2652b4e13790a63
Xi He <xh243@cornell.edu>
2018-01-02 23:51:57 -0800

    updates

--- node/trade.js
+++ node/trade.js
@@ -1,13 +1,16 @@
 'use strict'
 const fs = require("fs");
 const Gdax = require('gdax');
-
+const publicClient = new Gdax.PublicClient('LTC-USD');
 const apiURI = 'https://api.gdax.com';
 const sandboxURI = 'https://api-public.sandbox.gdax.com';
 
-global._base = __dirname + '/../';
+global._base = __dirname + "";
+console.log(global._base);
+const content = fs.readFileSync(global._base + "/encript.json");
+const Provider = require(global._base + "main.js");
+const engine = new Provider();
 
-const content = fs.readFileSync(global._base+"/node/encript.json");
 const obj = JSON.parse(content);
 const key = obj.key;
 const secret = obj.value;
@@ -17,12 +20,29 @@ const authedClient = new Gdax.AuthenticatedClient(key, secret, passphrase, apiUR
 
 const sellParams = {
     'price': '13600.0', // USD
-    'size': '0.0001',  // BTC
+    'size': '0.0001', // BTC
     'product_id': 'BTC-USD',
 };
 
-authedClient.sell(sellParams)
-.then((data)=>{
-    console.log(data);
-})
 
+authedClient.sell(sellParams)
+    .then((data) => {
+        console.log(data);
+    })
+
+
+
+var getPriceAsync = function() {
+    return publicClient.getProductTicker()
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            // handle the error
+        });
+}
+
+while(true){
+
+
+}
